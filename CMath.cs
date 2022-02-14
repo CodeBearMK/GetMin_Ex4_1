ecc45f9b694ec8def88b2456aec0a2543b003cfa
cat32tw <mike1993xd@gmail.com>
2022-02-14 19:33:18 +0800

    第四章習題1

---
+++ CMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMin_Ex4_1
+{
+    internal class CMath
+    {
+        public int GetMin(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num2;
+            }
+            else
+            {
+                return num1;
+            }
+        }
+
+        public int GetMin(int[] numAry)
+        {
+            int min = numAry[0];
+            for (int i = 1; i < numAry.Length; i++)
+            {
+                if (numAry[i] < min)
+                {
+                    min = numAry[i];
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GetMin_Ex4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CMath cal1 = new CMath();
+            int n1 = -20, n2 = 15;
+            Console.WriteLine("{0},{1} 較小值為 {2}", n1, n2, cal1.GetMin(n1, n2));
+
+            CMath cal2 = new CMath();
+            int[] nAry = { -10, 0, 30, -12, 25 };
+            Console.WriteLine("{0},{1},{2},{3},{4} 中最小值為 {5}",
+                                nAry[0], nAry[1], nAry[2], nAry[3], nAry[4], cal2.GetMin(nAry));
+            Console.Read();
+        }
+    }
+}
